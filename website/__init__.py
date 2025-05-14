df6dbaabab231b2e57f82e2167c043b0b8cc0c93
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2025-05-15 01:06:28 +0530

    fix: correct mail password assignment and use environment variable for sender email

--- website/__init__.py
+++ website/__init__.py
@@ -57,7 +57,7 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = ADMIN_EMAIL
-    app.config['MAIL_PASSWORD'] = ADMIN_EMAIL
+    app.config['MAIL_PASSWORD'] = ADMIN_PASSWORD
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
 
