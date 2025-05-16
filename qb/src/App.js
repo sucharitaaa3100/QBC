e89f0bfd3e8224dea0c20cd26cff8d5731b8ed0c
Sucharita Nandy <sucharitan654@gmail.com>
2025-05-16 17:30:29 +0530

    updated 4 files

--- qb/src/App.js
+++ qb/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminLayout from './layouts/AdminLayout';
-import AdminDashboard from './pages/AdminDashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLayout from "./layouts/AdminLayout";
+import AdminDashboard from "./pages/AdminDashboard";
+import AddQuiz from "./pages/AddQuiz";
+import AddChapter from "./pages/AddChapter";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="dashboard" element={<AdminDashboard />} />
-         
+        <Route path="/" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="subject/:subjectId/add-quiz/:chapterId" element={<AddQuiz />} />
+          <Route path="subject/:subjectId/add-chapter" element={<AddChapter />} />
         </Route>
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
+
--- qb/src/components/Navbar.js
+++ qb/src/components/Navbar.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
       <div className="container">
-        <Link className="navbar-brand" to="/admin/dashboard">Admin Panel</Link>
+        <Link className="navbar-brand" to="/">Admin Panel</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon" />
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/admin/dashboard">Dashboard</Link>
+              <Link className="nav-link" to="/">Dashboard</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/admin/about">About</Link>
+              <Link className="nav-link" to="/subject/1/add-quiz/2">Add Quiz</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/admin/analytics">Analytics</Link>
+              <Link className="nav-link" to="/subject/1/add-chapter">Add Chapter</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/logout">Logout</Link>
